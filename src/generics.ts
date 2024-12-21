b1fd4937c03f6a4153fad00463fd07ab5ee1305a
shayan heidary <hdrydeveloper@gmail.com>
2024-12-21 13:53:32 +0100

    generic samples

--- src/generics.ts
+++ src/generics.ts
@@ -25,22 +25,6 @@ interface PhoneInfo {
   num: string;
 }
 
-//* Goal
-
-// const phoneDict = {
-//     "0001": {
-//       customerId: "0001",
-//       areaCode: "321",
-//       num: "123-4566",
-//     },
-//     "0002": {
-//       customerId: "0002",
-//       areaCode: "174",
-//       num: "142-3626",
-//     },
-//     /*... and so on */
-//   }
-
 function listToDict<T>(
   list: T[],
   idGenerator: (arg: T) => string
@@ -55,3 +39,64 @@ function listToDict<T>(
   return dict;
 }
 const result = listToDict(phoneList, (item) => item.customerId);
+
+console.log(result);
+
+const fruits = {
+  apple: { color: "red", mass: 100 },
+  grape: { color: "red", mass: 5 },
+  banana: { color: "yellow", mass: 183 },
+  lemon: { color: "yellow", mass: 80 },
+  pear: { color: "green", mass: 178 },
+  orange: { color: "orange", mass: 262 },
+  raspberry: { color: "red", mass: 4 },
+  cherry: { color: "red", mass: 5 },
+};
+
+interface Dict<T> {
+  [k: string]: T;
+}
+
+function mapDict<T, S>(
+  inputDict: Dict<T>,
+  mapFunction: (original: T, key: string) => S
+): Dict<S> {
+  const outDict: Dict<S> = {};
+  for (let k of Object.keys(inputDict)) {
+    const thisVal = inputDict[k];
+    outDict[k] = mapFunction(thisVal, k);
+  }
+  return outDict;
+}
+
+const fruitsWithKgMass = mapDict(fruits, (fruit, name) => ({
+  ...fruit,
+  kg: 0.001 * fruit.mass,
+  name,
+}));
+
+console.log("res", fruitsWithKgMass);
+const lemonName: string = fruitsWithKgMass.lemon.name;
+
+//* Generic functions
+
+const identity = <T>(arg: T): T => arg;
+
+const result1 = identity<number>(42);
+
+function filterDict<T>(
+  inputDiict: Dict<T>,
+  filterFunction: (value: T) => boolean
+): Dict<T> {
+  const outPutDict: Dict<T> = {};
+  for (let key of Object.keys(inputDiict)) {
+    const temp = inputDiict[key];
+
+    if (filterFunction(temp)) {
+      outPutDict[key] = temp;
+    }
+  }
+  return outPutDict;
+}
+
+const redFruits = filterDict(fruits, (fruit) => fruit.color === "red");
